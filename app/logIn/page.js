7019e9b499c244c87f14e0fe8f33ab596026ba6c
mirmohsin0001 <mirmohsin0001@gmail.com>
2025-01-28 23:44:30 +0530

    first commit

---
+++ app/logIn/page.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto flex flex-wrap items-center">
+                    <div className="lg:w-3/5 md:w-1/2 md:pr-16 lg:pr-0 pr-0">
+                        <h1 className="title-font font-medium text-3xl text-gray-900">Log In next level shoindcgoitch ethical authentic, poko scenester</h1>
+                        <p className="leading-relaxed mt-4">Poke slow-carb mixtape knausgaard, typewriter street art gentrify hammock starladder roathse. Craies vegan tousled etsy austin.</p>
+                    </div>
+                    <form method='post' action={console.log("execute data posting logic for log in")} className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
+                        <h2 className="text-gray-900 text-lg font-medium title-font mb-5">Log In</h2>
+                        <div className="relative mb-4">
+                            <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                            <input type="email" id="email" name="email" required className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <label htmlFor="password" className="leading-7 text-sm text-gray-600">Password</label>
+                            <input type="password" id="password" name="password" required className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+
+                        </div>
+                        <div className="relative mb-4">
+                        </div>
+                        <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                        <p className="text-xs text-gray-500 mt-3">Not a user? <Link href={'/signUp'} className='text-indigo-500 cursor-pointer' >Sign Up</Link> </p>
+                    </form>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default page
