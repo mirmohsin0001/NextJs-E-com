984755e8d4407ecf1b689219acd77d737512b265
mirmohsin0001 <mirmohsin0001@gmail.com>
2025-02-15 00:32:01 +0530

    added a heading to the homepage products section

--- app/page.js
+++ app/page.js
@@ -8,8 +8,12 @@ import Tesmonials from "./components/Tesmonials";
 export default function Home() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <Hero />
+      <div className="flex flex-col text-center w-full mt-16 mb-16">
+        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">FIND YOUR FAVOURITES</h2>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Introducing Our Products</h1>
+      </div>
       <Products />
       <Features />
       <Tesmonials />
